6a4cedad17023a7db060103196527f01c58da99c
meggmaier <megg.maier@gmail.com>
2019-08-11 19:15:22 +0200

    srcipt.js updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
+  const apiRoot = 'https://gentle-inlet-57056.herokuapp.com/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
