665d0aeadebddcfed1c4db3b106d6e63bcce64a2
meggmaier <megg.maier@gmail.com>
2019-08-13 20:14:20 +0200

    trello apiroot change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://gentle-inlet-57056.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://api.trello.com/1/v1/trello/';
+  const trelloApiRoot = 'https://gentle-inlet-57056.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
